7b06999d8800f3b1c9284442054d4e799bf7644e
Jacqueline Guo <guojacqu@usc.edu>
2024-08-08 23:19:33 +0900

    function to get user input for RPS

--- file.js
+++ file.js
@@ -1,4 +1,4 @@
-function rps_play () {
+function getComputerChoice () {
     if (Math.random() <= (1 / 3)) {
         return "rock";
     }
@@ -10,6 +10,16 @@ function rps_play () {
     }
 }
 
-console.log(rps_play());
-console.log(rps_play());
-console.log(rps_play());
+function getHumanChoice () {
+    let userInput = prompt("Rock, Paper, or Scissors?");
+    
+    if (userInput == "rock" || userInput == "Rock") {
+        return "rock";
+    }
+    else if (userInput == "paper" || userInput == "Paper") {
+        return "paper";
+    }
+    else if (userInput == "scissors" || userInput == "Scissors") {
+        return "scissors";
+    }
+}
