e929a41831ad38c368f447a953003ce8f3132561
Jacqueline Guo <guojacqu@usc.edu>
2024-08-08 23:29:17 +0900

    Progress on playRound() function

--- file.js
+++ file.js
@@ -1,3 +1,8 @@
+
+var humanScore = 0;
+var computerScore = 0;
+
+
 function getComputerChoice () {
     if (Math.random() <= (1 / 3)) {
         return "rock";
@@ -13,13 +18,26 @@ function getComputerChoice () {
 function getHumanChoice () {
     let userInput = prompt("Rock, Paper, or Scissors?");
     
-    if (userInput == "rock" || userInput == "Rock") {
+    if (userInput == "rock") {
         return "rock";
     }
-    else if (userInput == "paper" || userInput == "Paper") {
+    else if (userInput == "paper") {
         return "paper";
     }
-    else if (userInput == "scissors" || userInput == "Scissors") {
+    else if (userInput == "scissors") {
         return "scissors";
+    }
+}
+
+function playRound (humanChoice, computerChoice) {
+    let human = humanChoice.toLowerCase();
+    let computer = computerChoice.toLowerCase();
+
+    if (human == computer) {
+        console.log(`Draw! ${human} and ${computer} are the same.` )
+    }
+    else if (human == "rock") {
+        if ()
+        console.log(`Draw! ${human} and ${computer} are the same.` )
     }
 }
