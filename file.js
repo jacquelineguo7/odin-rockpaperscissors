16ab3dbfa6815aaaece784e71e1eb785f3b09102
Jacqueline Guo <guojacqu@usc.edu>
2024-08-12 00:17:31 +0900

    complete playRound() function, which contains the logic to play a single round of RPS

--- file.js
+++ file.js
@@ -37,7 +37,34 @@ function playRound (humanChoice, computerChoice) {
         console.log(`Draw! ${human} and ${computer} are the same.` )
     }
     else if (human == "rock") {
-        if ()
-        console.log(`Draw! ${human} and ${computer} are the same.` )
+        if (computer == "paper") {
+            console.log("You lose! Paper beats Rock.")
+            computerScore++;
+        }
+        else {
+            console.log("You win! Rock beats Scissors.");
+            humanScore++;
+        }
+    }
+    else if (human == "paper") {
+        if (computer == "scissors") {
+            console.log("You lose! Scissors beats Paper.")
+            computerScore++;
+        }
+        else {
+            console.log("You win! Paper beats Rock.");
+            humanScore++;
+        }
+    }
+    else if (human == "scissors") {
+        if (computer == "rock") {
+            console.log("You lose! Rock beats Scissors.")
+            computerScore++;
+        }
+        else {
+            console.log("You win! Scissors beats Paper.");
+            humanScore++;
+        }
     }
 }
+
